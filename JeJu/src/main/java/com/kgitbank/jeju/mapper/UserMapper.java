77ffdd9f56d0ff5d225538b80a92ddcda4e7e98c
ohsoou <eoghkqks@naver.com>
2021-10-14 18:52:43 +0900

    add userMapper

---
+++ JeJu/src/main/java/com/kgitbank/jeju/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.kgitbank.jeju.mapper;
+
+import java.util.List;
+
+import com.kgitbank.jeju.dto.User;
+
+public interface UserMapper {
+	public List<User> listUsers();
+	public int addUser();
+}
