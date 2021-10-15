f00022f7cd31fa5e725a3cd6eddac5da4580989e
ohsoou <eoghkqks@naver.com>
2021-10-15 21:56:31 +0900

    mypage test

--- JeJu/src/main/java/com/kgitbank/jeju/controller/JejuController.java
+++ JeJu/src/main/java/com/kgitbank/jeju/controller/JejuController.java
@@ -1,7 +1,18 @@
 package com.kgitbank.jeju.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonParser;
+import com.kgitbank.jeju.dto.FamousRestaurant;
+import com.kgitbank.jeju.mapper.FamousRestaurantMapper;
 
 import lombok.extern.log4j.Log4j;
 
@@ -9,8 +20,24 @@ import lombok.extern.log4j.Log4j;
 @Controller
 public class JejuController {
 	
+	@Autowired
+	FamousRestaurantMapper restaurantMapper;
+	
 	@GetMapping("/login/callback")
 	public void callback() {
 		log.info("controller = callback");
+	}
+	
+	@GetMapping("/mypage")
+	public String mypage(HttpSession session, Model model) {
+		String id = (String) session.getAttribute("id");
+		if(id == null) {
+			return "redirect:/login";
+		}
+
+		
+		List<FamousRestaurant> restaurants = restaurantMapper.listByUser(id);
+		model.addAttribute("restaurants", restaurants);
+		return "/mypage/test";
 	}
 }
