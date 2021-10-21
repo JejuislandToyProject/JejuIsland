4fe425b78be26c35d25aa39b2577f2c44a55fc39
ohsoou <eoghkqks@naver.com>
2021-10-21 17:56:38 +0900

    add tourist pagination js

--- JeJu/src/main/webapp/resources/js/touristBoard.js
+++ JeJu/src/main/webapp/resources/js/touristBoard.js
@@ -1,5 +1,12 @@
-const cardBody = document.getElementById('card-body');
-const pagination = document.getElementById('pagination');
+const cardBody = document.getElementById('contents-body');
+const $pagination = $('#pagination');
+
+const cardPerPage = 8;
+let totalcards = 0;
+let cards = [];
+let displayCards = [];
+let page = 1;
+let totalPages = 0;
 
 makeRequest();
 
@@ -8,7 +15,7 @@ function makeRequest() {
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = setVariable;
 
-    xhttp.open('GET', '/jeju/board/tourist', true);
+    xhttp.open('GET', '/jeju/touristBoard', true);
     xhttp.send();
 }
 
@@ -16,29 +23,94 @@ function setVariable() {
     if(xhttp.readyState === XMLHttpRequest.DONE) {
         if(xhttp.status === 200) {
             console.dir(xhttp);
-            records = JSON.parse(xhttp.responseText);
-            console.log(records);
-            totalRecords = records.length;
-            totalPages = Math.ceil(totalRecords/recordPerPage);
+            cards = JSON.parse(xhttp.responseText);
+            console.log(cards);
+            totalcards = cards.length;
+            totalPages = Math.ceil(totalcards/cardPerPage);
             
             applyPagination();
         }
     }
 }
 
-const generateCard = () => {
-    tableBody.innerHTML = "";
 
-    for(let i = 0; i < displayRecords.length; i++) {
-        addRecord(displayRecords[i]);
+const generateCard = () => {
+    cardBody.innerHTML = "";
+
+    for(let i = 0; i < displayCards.length; i++) {
+        addCard(displayCards[i]);
     }
 }
-const addRecord = (record) => {
-    const div = document.createElement('div');
-    div.innerHTML += `<td>${record.image }</td>`;
-    div.innerHTML += `<td>${record.name }</td>`;
-    div.innerHTML += `<td>${record.description }</td>`;
-    cardBody.appendChild(div);
+const addCard = (card) => {
+    const cardGroup = document.createElement('div');
+    const cardBorder = document.createElement('div');
+
+    cardGroup.classList.add('card-group');
+    cardBorder.classList.add('card');
+    cardBorder.classList.add('shadow-sm');
+    cardBorder.classList.add('rounded-3');
+
+    
+    cardBorder.appendChild(addCardHeader(card));
+    cardBorder.appendChild(addCardBody(card));
+    cardBorder.appendChild(addIconPart());
+
+    cardGroup.appendChild(cardBorder);
+
+    cardBody.appendChild(cardGroup);
+}
+const addCardHeader = (card) => {
+    const cardHeader = document.createElement('div');
+    cardHeader.classList.add('card-header');
+    cardHeader.classList.add('p-0');
+    cardHeader.classList.add('mx-3');
+    cardHeader.classList.add('mt-3');
+    cardHeader.classList.add('position-relative');
+    cardHeader.classList.add('z-index-1');
+
+    cardHeader.innerHTML += `<a href="javascript:;" class="d-block"> 
+                            <img src=${card.image } class="img-fluid border-radius-lg"> 
+                            </a>`;
+    return cardHeader;
+}
+const addCardBody = (card) => {
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+    cardBody.classList.add('pt-2');
+
+    cardBody.innerHTML += `<a href="javascript:;" class="card-title h5 d-block text-darker text-center mt-3" id="card-title">
+                                ${card.name }
+                            </a>
+                            <p class="card-description text-center mb-4">
+                            <small class="text-muted">${card.description }</small>
+                            </p>`;
+    return cardBody;
+}
+
+const addIconPart = () => {
+    const outerDiv = document.createElement('div');
+    const innerDiv1 = document.createElement('div');
+    const innerDiv2 = document.createElement('div');
+
+    outerDiv.classList.add('author');
+    outerDiv.classList.add('align-items-center');
+
+    innerDiv1.classList.add('mt-6');
+    innerDiv2.classList.add('mt-2');
+
+    innerDiv1.setAttribute('id', 'icon');
+    innerDiv2.setAttribute('id', 'icon');
+
+    innerDiv1.innerHTML += '<i id="icon" onclick="changeThumbs()" class="far fa-thumbs-up"></i>';
+    innerDiv1.innerHTML += '<i class="far fa-thumbs-down"></i>';
+
+    innerDiv2.innerHTML += '<p>좋아요</p>';
+    innerDiv2.innerHTML += '<p>싫어요</p>';
+
+    outerDiv.appendChild(innerDiv1);
+    outerDiv.appendChild(innerDiv2);
+
+    return outerDiv;
 }
 
 
@@ -53,11 +125,11 @@ const applyPagination = ()=> {
         first: '',
         last: '',
         onPageClick: function(e, page) {
-            let displayRecordsIndex = Math.max(page -1, 0) * recordPerPage;
-            let endRecord = (displayRecordsIndex) + recordPerPage;
+            let displayCardsIndex = Math.max(page -1, 0) * cardPerPage;
+            let endCard = (displayCardsIndex) + cardPerPage;
 
-            displayRecords = records.slice(displayRecordsIndex, endRecord);
-            generateTable();
+            displayCards = cards.slice(displayCardsIndex, endCard);
+            generateCard();
         }
     });
 }
