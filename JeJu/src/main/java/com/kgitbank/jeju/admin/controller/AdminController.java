5c2a6aadf79bc4ea817f2e424c400efdd9e1703a
ohsoou <eoghkqks@naver.com>
2021-10-30 15:00:07 +0900

    separate process to service

--- JeJu/src/main/java/com/kgitbank/jeju/admin/controller/AdminController.java
+++ JeJu/src/main/java/com/kgitbank/jeju/admin/controller/AdminController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kgitbank.jeju.admin.dto.Main;
-import com.kgitbank.jeju.admin.mapper.MainMapper;
 import com.kgitbank.jeju.admin.service.AdminService;
 
 import lombok.extern.log4j.Log4j;
@@ -27,9 +26,6 @@ public class AdminController {
 	
 	@Autowired
 	AdminService adminService;
-	
-	@Autowired
-	MainMapper mainMapper;
 	
 	
 	@RequestMapping(value="/admin/login", method= {RequestMethod.GET, RequestMethod.POST})
@@ -48,30 +44,18 @@ public class AdminController {
 	public String user() {
 		return "/admin/utilities-user-ban";
 	}
+	
 	@RequestMapping(value="/admin/site", method= {RequestMethod.GET, RequestMethod.POST})
 	public String test(Model model) {
-		model.addAttribute("main", mainMapper.getList().get(0));
+		adminService.getMainInfo(model);
 		return "/admin/utilities-site";
 	}
 	
 	@RequestMapping(value="/admin/site/success", method=RequestMethod.POST, headers = ("content-type=multipart/*"))
 	public String addTourist(@RequestParam("imageFile") MultipartFile multi, 
 			@ModelAttribute("main") Main main, HttpServletRequest request) throws Exception{	
-		String fileName = multi.getOriginalFilename();
-		try {
-			String root_path = request.getSession().getServletContext().getRealPath("/resources/img");  
-			root_path += File.separator + fileName;
-			File file =new File(root_path);
-		    multi.transferTo(file);
-		    log.info(file.getAbsolutePath());
-		    Main updateMain = new Main(main.getInfo_id(),
-		    							"./resources/img/"+fileName,
-		    							main.getTitle(), 
-		    							main.getSub_title());
-		    mainMapper.updateMainInfo(updateMain);
-		}catch(Exception e) {
-			log.info("error : "+e);
-		}
+		
+		adminService.uploadMainInfo(multi, main, request);
 		return "redirect:/admin/site";
 	}
 	
--- JeJu/src/main/java/com/kgitbank/jeju/admin/service/AdminService.java
+++ JeJu/src/main/java/com/kgitbank/jeju/admin/service/AdminService.java
@@ -3,11 +3,15 @@ package com.kgitbank.jeju.admin.service;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kgitbank.jeju.admin.dto.DailyVisit;
+import com.kgitbank.jeju.admin.dto.Main;
 import com.kgitbank.jeju.dto.User;
 
 public interface AdminService {
@@ -15,5 +19,7 @@ public interface AdminService {
 	public void getDashboardInfo(Model model);
 	public List<DailyVisit> getChartData();
 	public List<User> getUserData();
+	public void getMainInfo(Model model);
 	public void updateBan(User user);
+	public void uploadMainInfo(MultipartFile multi, Main main, HttpServletRequest request);
 }
