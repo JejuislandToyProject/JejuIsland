ef7f42f1e1fc7c1ba242f29d8064987bdcb13074
ohsoou <eoghkqks@naver.com>
2021-11-02 19:57:37 +0900

    fix rest error

--- JeJu/src/main/java/com/kgitbank/jeju/controller/CourseRestController.java
+++ JeJu/src/main/java/com/kgitbank/jeju/controller/CourseRestController.java
@@ -24,68 +24,68 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class CourseRestController {
 
-	@Autowired
-	AreaXMLMapper areaMapper;
-	
-	@Autowired
-	TouristSpotMapper spotMapper;
-	
-	@Autowired
-	FamousRestaurantMapper matzipMapper;
-	
-	@Autowired
-	SpotAndRestaurantMapper spotAndMatzipMapper;
-	
-	@Autowired
-	WeatherServiceImpl weatherServiceImpl;
-	
-	@GetMapping(value="/course/{title}", produces = "application/json; charset=UTF-8")
-	public List<?> getList(@PathVariable("title") String title) {
+    @Autowired
+    AreaXMLMapper areaMapper;
+    
+    @Autowired
+    TouristSpotMapper spotMapper;
+    
+    @Autowired
+    FamousRestaurantMapper matzipMapper;
+    
+    @Autowired
+    SpotAndRestaurantMapper spotAndMatzipMapper;
+    
+    @Autowired
+    WeatherServiceImpl weatherServiceImpl;
+    
+    @GetMapping(value="/course/{title}", produces = "application/json; charset=UTF-8")
+    public List<?> getList(@PathVariable("title") String title) {
 
-		if (title.equals("matzip")) {
-			return matzipMapper.famousList();
-		} else {
-			return spotMapper.courseList();
-		}
-	}
-	
-	@GetMapping(value="/select/{title}", produces = "application/json; charset=UTF-8")
-	public SpotAndRestaurant getSelecte(@PathVariable("title") String title) {
-		
-		return spotAndMatzipMapper.getDTOSpotAndRestaurant(title);
+        if (title.equals("matzip")) {
+            return matzipMapper.famousList();
+        } else {
+            return spotMapper.courseList();
+        }
+    }
+    
+    @GetMapping(value="/select/{title}", produces = "application/json; charset=UTF-8")
+    public SpotAndRestaurant getSelecte(@PathVariable("title") String title) {
+        
+        return spotAndMatzipMapper.getDTOSpotAndRestaurant(title);
 
-	}
-	
-	@GetMapping(value="/search/{textValue}", produces = "application/json; charset=UTF-8")
-	public List<SpotAndRestaurant> getSearch(@PathVariable("textValue") String textValue) {
-			
-		return spotAndMatzipMapper.getListBySearchTitle(textValue);
-	}
-	
-	@GetMapping(value="/modalSearch/{title}", produces = "application/json; charset=UTF-8")
-	public List<SpotAndRestaurant> getSearch2(@PathVariable("title") String title) {
-		
-		String names[] = title.split(",");
-		List<SpotAndRestaurant> list = new ArrayList<>();
-		
-		for(int i =0; i < names.length; ++i) {
-			list.add((SpotAndRestaurant) spotAndMatzipMapper.getDTOSpotAndRestaurant(names[i]));
-			
-		}
-		return list;	
-	}
+    }
+    
+    @GetMapping(value="/search/{textValue}", produces = "application/json; charset=UTF-8")
+    public List<SpotAndRestaurant> getSearch(@PathVariable("textValue") String textValue) {
+            
+        return spotAndMatzipMapper.getListBySearchTitle(textValue);
+    }
+    
+    @GetMapping(value="/modalSearch/{title}", produces = "application/json; charset=UTF-8")
+    public List<SpotAndRestaurant> getSearch2(@PathVariable("title") String title) {
+        
+        String names[] = title.split(",");
+        List<SpotAndRestaurant> list = new ArrayList<>();
+        
+        for(int i =0; i < names.length; ++i) {
+            list.add((SpotAndRestaurant) spotAndMatzipMapper.getDTOSpotAndRestaurant(names[i]));
+            
+        }
+        return list;    
+    }
 
-	@GetMapping(value ="/GetAreas/{title}", produces ="application/json; charset=UTF-8")
-	public Area getIntroduce(@PathVariable("title") String title) { 
-		
-		return areaMapper.getIntroduce(title); 
-	}
-	
-	
-	@GetMapping(value = "/weather", produces ="application/json; charset=UTF-8")
-	public Weather getWeather(Model model) throws Exception {
-		
-		return weatherServiceImpl.getWeather();	
-	}	
-	
+    @GetMapping(value ="/GetAreas/{title}", produces ="application/json; charset=UTF-8")
+    public Area getIntroduce(@PathVariable("title") String title) { 
+        
+        return areaMapper.getIntroduce(title); 
+    }
+    
+    
+    @GetMapping(value = "/weather", produces ="application/json; charset=UTF-8")
+    public Weather getWeather(Model model) throws Exception {
+        
+        return weatherServiceImpl.getWeather();    
+    }    
+    
 }
